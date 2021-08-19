5fa595c1217b442810718aae8111dfaf49588469
harutaka <10650294+harutaka@users.noreply.github.com>
2021-08-19 11:42:52 +0900

    change JS to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Header from '../component/header'
 import Mainvisual from '../component/mainvisual'
 import About from '../component/about'
